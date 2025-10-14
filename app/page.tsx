0b6fdc32ae45caab638571ce61a3f5ac32343a12
Jan Konieczek <koleks92@gmail.com>
2025-10-14 14:24:24 +0200

    feat: Reading from supabse, simple data presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
 import { createClient } from "../utils/supabase/server";
 
+type Scores = {
+    id: number;
+    created_at: string;
+    score: number;
+    player_id: number;
+};
+
 export default async function Page() {
     const supabase = await createClient();
 
-    const { data: todos } = await supabase.from("todos").select();
+    const { data } = await supabase.from("Scores").select("*");
+
+    const scores: Scores[] = data ?? [];
 
     return (
-        <ul>
-            {todos?.map((todo) => (
-                <li>{todo}</li>
-            ))}
-        </ul>
+        <div className="m-4 justify-center">
+            <div className="flex justify-center">Players Score List</div>
+            <div className="m-6">
+                <ul>
+                    {scores.map((score) => (
+                        <li key={score.id}>
+                            {score.created_at} {score.score} {score.player_id}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
 }
