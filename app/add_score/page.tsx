6338ca77fe2f2fe88fc2e9abf6aa1950ff4e3a4d
Jan Konieczek <koleks92@gmail.com>
2025-10-17 18:13:34 +0200

    feat: Initial version with go back and title

--- app/add_score/page.tsx
+++ app/add_score/page.tsx
@@ -1,13 +1,17 @@
-import { createSimpleClient } from "@/utils/supabase/server"
-
+import { createSimpleClient } from "@/utils/supabase/server";
 
 export default function AddScore() {
-    const supabase = createSimpleClient()
+    const supabase = createSimpleClient();
 
     return (
-        <div className="m-6 justify-center align-center">
-            Add Player
-            <a href="/">Go Back</a>
+        <div className="m-4 justify-center align-center">
+            <div className="flex m-2 justify-center text-3xl font-bold">
+                Add Score
+            </div>
+            <div className="flex justify-center">
+                <a href="/">Go Back</a>
+            </div>
+
         </div>
-    )
-};
+    );
+}
